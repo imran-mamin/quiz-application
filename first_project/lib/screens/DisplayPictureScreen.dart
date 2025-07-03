91a1d749c549af245a42d3ce168cb98f64d12ebb
Imran Mamin <imran@mm.st>
2025-07-03 14:57:32 +0300

    Remove redundant imports from DisplayPictureScreen.dart.

--- first_project/lib/screens/DisplayPictureScreen.dart
+++ first_project/lib/screens/DisplayPictureScreen.dart
@@ -1,14 +1,6 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:flash_card/flash_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
-import 'package:get/get.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
-import 'package:hive_ce_flutter/hive_flutter.dart';
-import 'package:camera/camera.dart';
 
 // A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatelessWidget {
